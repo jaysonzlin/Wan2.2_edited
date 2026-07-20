c043181958afeaeeb81fb8ad9ffb5a3deef541e0
Jayson Lin <jaysonlin@Jaysons-Mac-mini-5.local>
2026-07-19 21:00:58 -0400

    fix: lazy load Wan module exports

--- wan/modules/__init__.py
+++ wan/modules/__init__.py
@@ -1,19 +1,33 @@
 # Copyright 2024-2025 The Alibaba Wan Team Authors. All rights reserved.
-from .attention import flash_attention
-from .model import WanModel
-from .t5 import T5Decoder, T5Encoder, T5EncoderModel, T5Model
-from .tokenizers import HuggingfaceTokenizer
-from .vae2_1 import Wan2_1_VAE
-from .vae2_2 import Wan2_2_VAE
+"""Lazy exports so lightweight Wan modules can run without a CUDA T5 import."""
+
+from importlib import import_module
 
 __all__ = [
-    'Wan2_1_VAE',
-    'Wan2_2_VAE',
-    'WanModel',
-    'T5Model',
-    'T5Encoder',
-    'T5Decoder',
-    'T5EncoderModel',
-    'HuggingfaceTokenizer',
-    'flash_attention',
+    "Wan2_1_VAE", "Wan2_2_VAE", "WanModel", "T5Model", "T5Encoder",
+    "T5Decoder", "T5EncoderModel", "HuggingfaceTokenizer", "flash_attention",
+    "PCFlowModel",
 ]
+
+_LAZY_EXPORTS = {
+    "flash_attention": (".attention", "flash_attention"),
+    "WanModel": (".model", "WanModel"),
+    "T5Model": (".t5", "T5Model"),
+    "T5Encoder": (".t5", "T5Encoder"),
+    "T5Decoder": (".t5", "T5Decoder"),
+    "T5EncoderModel": (".t5", "T5EncoderModel"),
+    "HuggingfaceTokenizer": (".tokenizers", "HuggingfaceTokenizer"),
+    "Wan2_1_VAE": (".vae2_1", "Wan2_1_VAE"),
+    "Wan2_2_VAE": (".vae2_2", "Wan2_2_VAE"),
+    "PCFlowModel": (".pc_flow", "PCFlowModel"),
+}
+
+
+def __getattr__(name):
+    try:
+        module_name, attribute = _LAZY_EXPORTS[name]
+    except KeyError as error:
+        raise AttributeError(f"module {__name__!r} has no attribute {name!r}") from error
+    value = getattr(import_module(module_name, __name__), attribute)
+    globals()[name] = value
+    return value
