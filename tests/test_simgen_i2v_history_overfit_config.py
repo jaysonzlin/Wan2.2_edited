98c4b9466c10221cf27f939944287c7cae80464e
Jayson Lin <jaysonzlin@holygpu7c26206.rc.fas.harvard.edu>
2026-09-01 18:46:28 -0400

    config: align SimGen I2V training schedule

--- tests/test_simgen_i2v_history_overfit_config.py
+++ tests/test_simgen_i2v_history_overfit_config.py
@@ -7,6 +7,8 @@ def test_config_has_native_history_overfit_settings() -> None:
     assert config["data"]["sample_root"].endswith("sample_0/view_0")
     assert (config["data"]["width"], config["data"]["height"]) == (480, 480)
     assert config["training"]["max_train_steps"] == 10_000
+    assert config["training"]["num_train_timesteps"] == 10_000
+    assert config["training"]["lr_scheduler"] == "constant"
     assert config["training"]["checkpoint_every_steps"] == 500
     assert config["training"]["visualization_every_steps"] == 500
     assert config["training"]["checkpoints_total_limit"] == 2
